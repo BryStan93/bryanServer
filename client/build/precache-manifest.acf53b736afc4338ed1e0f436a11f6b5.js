dd410db7eb1f62f29c259c1184100e5a5eb23bb5
Bryan Stanley Garcia <32469407+BryStan93@users.noreply.github.com>
2020-05-06 21:05:36 -0600

    Add files via upload

---
+++ client/build/precache-manifest.acf53b736afc4338ed1e0f436a11f6b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea70d5901a412131030ac85fd7673fc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3dbb1d9490733abc62",
+    "url": "/static/css/2.658eb2da.chunk.css"
+  },
+  {
+    "revision": "7b3dbb1d9490733abc62",
+    "url": "/static/js/2.0a214a5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0a214a5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a38f1e379f73ed7265c",
+    "url": "/static/js/main.487e06ae.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
